cd30738859bd1de2e80a8619e870258222827528
Ekrem Karatas <ekremk95@gmail.com>
2018-06-02 14:37:31 +0300

    add postgres crate

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![feature(plugin)]
 #![plugin(rocket_codegen)]
+extern crate postgres;
 extern crate rocket;
 extern crate rocket_contrib;
 extern crate serde;
@@ -8,6 +9,7 @@ extern crate serde_json;
 #[macro_use]
 extern crate serde_derive;
 
+use postgres::{Connection, TlsMode};
 use rocket::response::NamedFile;
 use rocket_contrib::Json;
 use std::path::Path;
@@ -22,6 +24,38 @@ fn index() -> &'static str {
 
 #[get("/user/<id>")]
 fn user(id: usize) -> Json<User> {
+    let conn =
+        Connection::connect("postgres://postgres:pass@localhost:5432", TlsMode::None).unwrap();
+
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS users (
+        id   SERIAL PRIMARY KEY,
+        name VARCHAR NOT NULL,
+        data BYTEA
+    )",
+        &[],
+    ).unwrap();
+
+    let me = User {
+        id: 0,
+        name: "Ekrem".to_string(),
+        data: None,
+    };
+
+    conn.execute(
+        "INSERT INTO users(name, data) VALUES ($1, $2)",
+        &[&me.name, &me.data],
+    ).unwrap();
+
+    for row in &conn.query("SELECT id, name, data FROM users", &[]).unwrap() {
+        let user = User {
+            id: row.get(0),
+            name: row.get(1),
+            data: row.get(2),
+        };
+        println!("{}: {}", user.id, user.name);
+    }
+
     from_id(id)
 }
 
--- src/user.rs
+++ src/user.rs
@@ -12,16 +12,17 @@ use rocket_contrib::Json;
 
 #[derive(Serialize, Deserialize)]
 pub struct User {
+    pub id: i32,
     pub name: String,
-    pub age: u16,
+    pub data: Option<Vec<u8>>,
 }
 
 impl<'r> Responder<'r> for User {
     fn respond_to(self, _: &Request) -> response::Result<'r> {
         Response::build()
-            .sized_body(Cursor::new(format!("{}:{}", self.name, self.age)))
+            .sized_body(Cursor::new(format!("{}:{}", self.id, self.name)))
             .raw_header("X-User-Name", self.name)
-            .raw_header("X-User-Age", self.age.to_string())
+            // .raw_header("X-User-Age", self.age.to_string())
             .header(ContentType::JSON)
             .ok()
     }
@@ -29,7 +30,8 @@ impl<'r> Responder<'r> for User {
 
 pub fn from_id(_: usize) -> Json<User> {
     Json(User {
+        id: 0,
         name: "eko".to_string(),
-        age: 16,
+        data: None,
     })
 }
