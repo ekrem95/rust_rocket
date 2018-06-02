ea74e4aaf7a15d3efdc7fa4ecf055b0ce6e97a57
Ekrem Karatas <ekremk95@gmail.com>
2018-06-02 12:52:13 +0300

    handle favicon, robotx.txt

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,18 @@
 #![plugin(rocket_codegen)]
 extern crate rocket;
 extern crate rocket_contrib;
-extern crate rust_rocket;
 extern crate serde;
 extern crate serde_json;
 
+#[macro_use]
+extern crate serde_derive;
+
+use rocket::response::NamedFile;
 use rocket_contrib::Json;
-use rust_rocket::{from_id, User};
+use std::path::Path;
+
+mod user;
+use user::{from_id, User};
 
 #[get("/")]
 fn index() -> &'static str {
@@ -19,9 +25,19 @@ fn user(id: usize) -> Json<User> {
     from_id(id)
 }
 
+#[get("/favicon.ico")]
+fn favicon() -> Option<NamedFile> {
+    NamedFile::open(Path::new("./pub/favicon.ico")).ok()
+}
+
+#[get("/robots.txt")]
+fn robots() -> Option<NamedFile> {
+    NamedFile::open(Path::new("./pub/robots.txt")).ok()
+}
+
 fn main() {
     rocket::ignite()
-        .mount("/", routes![index])
+        .mount("/", routes![index, favicon, robots])
         .mount("/api", routes![user])
         .launch();
 }
--- src/user.rs
+++ src/user.rs
@@ -1,12 +1,7 @@
-#![feature(plugin)]
-#![plugin(rocket_codegen)]
 extern crate rocket;
 extern crate rocket_contrib;
 extern crate serde;
 extern crate serde_json;
-
-#[macro_use]
-extern crate serde_derive;
 
 use std::io::Cursor;
 
