cf898f460d6de4f46a1e07faa126ce79c0ea41f3
Ekrem Karatas <ekremk95@gmail.com>
2018-06-23 12:33:52 +0300

    upload text file

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,9 @@ extern crate serde_derive;
 
 use rocket::response::status::NotFound;
 use rocket::response::{content, NamedFile};
+use rocket::Data;
 use rocket_contrib::Json;
+use std::fs;
 use std::path::Path;
 
 mod pg;
@@ -33,6 +35,28 @@ fn get_user(id: i32) -> Result<Json<User>, NotFound<content::Json<&'static str>>
     User::get_user(id)
 }
 
+#[post("/upload", format = "text/plain", data = "<data>")]
+fn upload(data: Data) -> Result<String, String> {
+    let dir_name = "./uploads";
+    let filename = "upload.txt";
+
+    match data
+        .stream_to_file(dir_name.to_owned() + "/" + filename)
+        .map(|n| n.to_string())
+    {
+        Result::Ok(_) => Ok("success".to_string()),
+        Result::Err(err) => {
+            if err.to_string() == "No such file or directory (os error 2)" {
+                return match fs::create_dir_all(dir_name) {
+                    Result::Ok(_) => Ok("dir_added".to_string()),
+                    Result::Err(_) => Err("Unknown error".to_string()),
+                };
+            }
+            Err("Unknown error".to_string())
+        }
+    }
+}
+
 #[get("/favicon.ico")]
 fn favicon() -> Option<NamedFile> {
     NamedFile::open(Path::new("./pub/favicon.ico")).ok()
@@ -48,6 +72,6 @@ fn main() {
 
     rocket::ignite()
         .mount("/", routes![index, favicon, robots])
-        .mount("/api", routes![add_user, get_user])
+        .mount("/api", routes![add_user, get_user, upload])
         .launch();
 }
